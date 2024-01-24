3814cdbbd3d92014fcf22ba7198f64f087f6a383
nine-chairs <havlik9@gmail.com>
2024-01-24 19:33:21 +0100

    created basic styling

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -2,53 +2,83 @@ import React from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import Calendar from '../Calendar/Calendar';
 import useViewModel from './viewModel';
+import './Calculator.css'
 
 const Calculator: React.FC = () => {
 
   const viewModel = useViewModel()
 
   return (
-    <div>
-      <label>
-        Cart Value (in euros):
-        <input
-          type="number"
-          value={viewModel.state.cartValueInput}
-          onChange={(e) => viewModel.handleInputChange(e, 'cartValueInput')}
-          min="0"
-        />
-      </label>
-      <br />
-      <label>
-        Delivery Distance (in meters):
-        <input
-          type="number"
-          value={viewModel.state.deliveryDistanceInput}
-          onChange={(e) => viewModel.handleInputChange(e, 'deliveryDistanceInput')}
-          min="0"
-        />
-      </label>
-      <br />
-      <label>
-        Number of items:
-        <input
-          type="number"
-          value={viewModel.state.numberOfItemsInput}
-          onChange={(e) => viewModel.handleInputChange(e, 'numberOfItemsInput')}
-          min="0"
-        />
-      </label>
-      <br />
-      <label>
-        Date and Time:
-        <Calendar
-          selectedDate={new Date(viewModel.state.dateTime)}
-          onDateChange={viewModel.handleDateChange}
-          onFocus={viewModel.handleDatepickerFocus}
-          onBlur={viewModel.handleDateBlur}
-          onReset={viewModel.resetDateTime}
-        />
-      </label>
+    <div className='calculatorContainer'>
+      <div className='inputContainer'>
+        <div className='inputLabelContainer'>
+          <label>
+            Cart Value:
+          </label>
+        </div>
+        <div className='inputFieldContainer'>
+          <input
+            className='inputField'
+            type="number"
+            value={viewModel.state.cartValueInput}
+            onChange={(e) => viewModel.handleInputChange(e, 'cartValueInput')}
+            min="0"
+          />
+        </div>
+      </div>
+
+      <div className='inputContainer'>
+        <div className='inputLabelContainer'>
+          <label>
+            Delivery Distance:
+          </label>
+        </div>
+        <div className='inputFieldContainer'>
+          <input
+            className='inputField'
+            type="number"
+            value={viewModel.state.deliveryDistanceInput}
+            onChange={(e) => viewModel.handleInputChange(e, 'deliveryDistanceInput')}
+            min="0"
+          />
+        </div>
+      </div>
+
+      <div className='inputContainer'>
+        <div className='inputLabelContainer'>
+          <label>
+            Number of items:
+          </label>
+        </div>
+        <div className='inputFieldContainer'>
+          <input
+            className='inputField'
+            type="number"
+            value={viewModel.state.numberOfItemsInput}
+            onChange={(e) => viewModel.handleInputChange(e, 'numberOfItemsInput')}
+            min="0"
+          />
+        </div>
+      </div>
+
+      <div className='inputContainer'>
+        <div className='inputLabelContainer'>
+          <label>
+            Date and Time:
+          </label>
+        </div>
+        <div className='inputFieldContainer'>
+          <Calendar
+            selectedDate={new Date(viewModel.state.dateTime)}
+            onDateChange={viewModel.handleDateChange}
+            onFocus={viewModel.handleDatepickerFocus}
+            onBlur={viewModel.handleDateBlur}
+            onReset={viewModel.resetDateTime}
+          />
+        </div>
+      </div>
+
+
       <br />
       <button onClick={viewModel.calculateDeliveryFee}>Calculate Fee</button>
       <br />
--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,5 +1,3 @@
-// Inside Calendar.tsx
-
 import React from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
